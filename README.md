f460f0241831fc752af53b413d9bad076f275142
Thorsten Zoerner <me@thorsten-zoerner.com>
2023-10-09 14:36:02 +0200

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # quivr-brain-manager
 Node Module to work with knowledge/brains in Quivr.
+
+## Motiviation
+During AI enginerring we required a method to simply re-creation of a knowledge-space based on a set of URLs. 
+
+## Usage
+
+### Add URL to default Brain
+```Javascript
+const Manager = require("quivr-brain-manager");
+
+const config = {
+    quivr_api_key:'<INSERT_YOUR_KEY_HERE>'
+};
+
+const manager = new Manager(config);
+
+const app = async function() {
+    console.log(await manager.crawl('Default brain','https://corrently.energy/'));
+};
+
+app();
+```
+
+### Add URL to on premise installation and custom brain
+```Javascript
+const Manager = require("quivr-brain-manager");
+
+const config = {
+    quivr_api_key:'<INSERT_YOUR_KEY_HERE>',
+    quivr_url:'https://yourInstallation.host.com'
+};
+
+const manager = new Manager(config);
+
+const app = async function() {
+    console.log(await manager.crawl('TestBrain','https://corrently.energy/'));
+};
+
+app();
+```
+
+Change TestBrain to the name or ID of your brain.
+
+## Maintainer / Imprint
+
+<addr>
+STROMDAO GmbH  <br/>
+Gerhard Weiser Ring 29  <br/>
+69256 Mauer  <br/>
+Germany  <br/>
+  <br/>
++49 6226 968 009 0  <br/>
+  <br/>
+kontakt@stromdao.com  <br/>
+  <br/>
+Handelsregister: HRB 728691 (Amtsgericht Mannheim)
+</addr>
+
+
+## LICENSE
+[Apache-2.0](LICENSE)
---
+++ index.js
@@ -0,0 +1,109 @@
+const axios = require("axios");
+
+module.exports = function(config) {
+    if((typeof config == 'undefined')||(config == null)){
+        throw new Error("Missing config. Specify at leas {config.quivr_api_key='YOUR_API_KEY'}");
+    }
+
+    if(typeof config.quivr_url == 'undefined' ) {
+        config.quivr_url = 'https://api.quivr.app';
+    }
+    if(typeof config.quivr_api_key == 'undefined') {
+        throw new Error("Missing quivr_api_key");
+    }
+    console.log(config);
+
+    const crawl = async function(brainId,url,settings){
+        const defaults = {
+            "url": url,
+            "js": false,
+            "depth": 1,
+            "max_pages": 100,
+            "max_time": 60
+        }
+
+        if((typeof settings == "undefined") || (settings == null)){
+            settings = defaults;
+            settings.url = url;
+        }
+        
+        const _brains = await brains();
+        let selectedBrain = null; 
+        for(let i=0;i<_brains.brains.length;i++) {
+            if(_brains.brains[i].id == brainId) {
+                selectedBrain = _brains.brains[i];
+            }
+            if(_brains.brains[i].name == brainId) {
+                selectedBrain = _brains.brains[i];
+            }
+        }
+        if(selectedBrain == null){
+            throw new Error("Brain not found");
+        }
+
+        const chat = await createChat(selectedBrain.name+"_crawl_"+new Date().getTime());
+        if(typeof chat.chat_id == "undefined"){
+            throw new Error("Chat not created for crawling");
+        }
+
+        let res = await axios.post(
+            "https://brain.tydids.com/crawl?brain_id="+selectedBrain.id+"&chat_id="+chat.chat_id,settings,{
+                headers:{
+                    'Authorization': 'Bearer '+config.quivr_api_key
+                }
+        });
+        return res.data;
+    }
+    /**
+         * Retrieves the brains from the quivr_url endpoint.
+         *
+         * @return {Promise<Object>} The brains data.
+    */
+    const brains = async function(){
+        const res = await axios.get(config.quivr_url + '/brains/',{
+            headers:{
+                'Authorization': 'Bearer '+config.quivr_api_key
+            }
+        });
+        return res.data;
+    }
+
+    /**
+     * Creates a chat with the given name.
+     *
+     * @param {string} name - The name of the chat.
+     * @return {Promise} A promise that resolves to the chat data.
+     */
+    const createChat = async function(name) {
+        const res = await axios.post(config.quivr_url + '/chat',{
+            "name":name
+        },{
+            headers:{
+                'Authorization': 'Bearer '+config.quivr_api_key
+            }
+        });
+        return res.data;
+    }
+
+    
+    const createBrain = async function(name) {
+        const res = await axios.post(config.quivr_url + '/brains/',{
+            "name":name
+        },{
+            headers:{
+                'Authorization': 'Bearer '+config.quivr_api_key
+            }
+        });
+        return res.data;
+    }
+    
+    return {
+        brains: brains,
+        createChat: createChat,
+        createBrain: createBrain,
+        crawl: crawl,
+        dumpConfig: function(){
+            return config;
+        }
+    }
+}
